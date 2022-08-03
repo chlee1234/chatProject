7856a6994b1e6b0c016358d451d5f27d214ed264
jjucc99 <59110017+jjucc99@users.noreply.github.com>
2022-08-04 00:02:47 +0900

    Update StompWebSocketConfig.java

--- src/main/java/com/chat/socket/config/StompWebSocketConfig.java
+++ src/main/java/com/chat/socket/config/StompWebSocketConfig.java
@@ -1,15 +1,18 @@
 package com.chat.socket.config;
 
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @EnableWebSocketMessageBroker
 @Configuration
+@RequiredArgsConstructor
 public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
     //endpoint를 /stomp로 하고, allowedOrigins를 "*"로 하면 페이지에서
     //Get /info 404 Error가 발생한다. 그래서 아래와 같이 2개의 계층으로 분리하고
     //origins를 개발 도메인으로 변경하니 잘 동작하였다.
